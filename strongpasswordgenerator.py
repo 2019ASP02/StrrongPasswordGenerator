38d7384e77770fc55b62499295dd2780f2f7cf22
Thuraisamy Vaitheeswaran <149567036+2019ASP02@users.noreply.github.com>
2024-12-23 19:04:35 +0530

    intilize libraey file

---
+++ strongpasswordgenerator.py
@@ -0,0 +1,20 @@
+
+import random
+import string
+
+
+ch1 = list(string.ascii_lowercase)
+ch2 = list(string.ascii_uppercase)
+ch3 = list(string.ascii_letters)
+pun = list(string.punctuation)
+num = list(string.digits)
+
+#print(ch1)
+#rint(ch2)
+#print(ch3)
+#print(pun)
+#print(num)
+
+wrd = input("Enter a word that exist in password : ")
+cod = int(input("Enter the number that exist in password : "))
+lgt = int(input("Enter the total lenght : "))
