5920796746a8e3f41c7b750aaf0f71e6d626c367
2019ASP02 <149567036+2019ASP02@users.noreply.github.com>
2024-12-23 19:30:57 +0530

    create the upper and lower case word from entered word

--- strongpasswordgenerator.py
+++ strongpasswordgenerator.py
@@ -14,7 +14,26 @@ num = list(string.digits)
 #print(ch3)
 #print(pun)
 #print(num)
+
 # input take from User 
 wrd = input("Enter a word that exist in password : ")  # user wish to put the word that exist in password
 cod = int(input("Enter the number that exist in password : ")) # user wish to put the nummber that exist in password
-lgt = int(input("Enter the total lenght : ")) # Total lenght of the password
+lgt = int(input("Enter the total lenght(greater than lenght of entered word and number) : ")) # Total lenght of the password
+
+lw = len(wrd) #lenght of word 
+lc = len(cod) # lenght of number
+lgt = lgt - lw - lc  # calculate the lenght to add extra charator
+
+wrd = list(wrd)
+cod = list(cod)
+
+spg = '' # stong password variable
+binary = '01'
+for i in range(0,lw-1,1):
+    n = int(random.choice(binary))
+    ow = wrd[i]
+    nw = ow.upper() if n == 1 else ow.lower()
+    wrd[i] = nw
+
+
+fs = random.choice(pun) # first symbol it's exist first of the password
